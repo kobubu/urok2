6567d74917838d7f6cb13dfaad6e3866e4c2fbdb
kobubu <79473433+kobubu@users.noreply.github.com>
2021-04-15 00:11:41 +0300

    Add files via upload

---
+++ algoritmurok1/task1/algo les1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Principal;
+
+namespace algo_les1
+{
+    class Program
+    {
+
+        //Написать консольное приложение.
+        //    Алгоритм реализовать отдельно в функции согласно блок-схеме.
+        //    Написать проверочный код в main функции.
+        //    Код выложить на GitHub.
+
+        public static void Function()
+        {
+            int d = 0;
+            int i = 2;
+            Console.WriteLine("Enter a number");
+            string input = Console.ReadLine();
+            bool result = int.TryParse(input, out int n);
+            if (result != true)
+            {
+                Console.WriteLine("Преобразование завершилось неудачно\n" +
+                                  "Введите другое число");
+
+            }
+            
+       
+
+            while (i < n)
+            {
+                if (n % i != 0)
+                {
+                    i++;
+                }
+                else
+                {
+                    d++;
+                    i++;
+                }   
+            }
+
+
+
+            if (d == 0)
+            {
+                Console.WriteLine("Вы ввели простое число");
+                
+            }
+            else
+            {
+                Console.WriteLine("Число, что вы ввели, не является простым");
+            }
+            
+        }
+
+ 
+        static void Main(string[] args)
+        {
+            Function();
+        }
+    }
+}
